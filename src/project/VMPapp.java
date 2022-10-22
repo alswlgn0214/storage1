804985216c110997daa750b4095aba2f7779965b
hsol19 <kimhgst919@naver.com>
2022-10-22 12:23:38 +0900

    Date

--- src/project/VMPapp.java
+++ src/project/VMPapp.java
@@ -1,32 +1,33 @@
-package project;
-
-import java.util.Scanner;
-
-public class VMPapp {
-    public static void main(String[] args) {
-        VMPsystem vmPsystem = new VMPsystem();
-        VMPui vmPui = new VMPui();
-
-
-        // 0 - main // 1 - buyer //2 - manager // 3 - exit
-        do {
-            switch (vmPsystem.location) {
-                case 0:
-                    vmPui.entry();
-                    vmPsystem.locate();
-                    break;
-                case 1:
-                    vmPui.showMachine();
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-            }
-        } while (VMPsystem.location != 3);
-
-        vmPui.bye();
-
-
-    }
-}
+//package project;
+//
+//import java.util.Scanner;
+//
+//public class VMPapp {
+//    public static void main(String[] args) {
+//        VMPsystem vmPsystem = new VMPsystem();
+//        VMPui vmPui = new VMPui();
+//
+//
+//        // 0 - main // 1 - buyer //2 - manager // 3 - exit
+//        do {
+//            switch (vmPsystem.location) {
+//                case 0:
+//                    vmPui.entry();
+//                    vmPsystem.locate();
+//                    break;
+//                case 1:
+//                    vmPui.showMachine();
+//                    break;
+//                case 2:
+//                    break;
+//                case 3:
+//                    break;
+//            }
+////        } while (VMPsystem.location != 3);
+//
+//            vmPui.bye();
+//
+//
+//        }
+//    }
+//}
--- src/project/VMmachine.java
+++ src/project/VMmachine.java
@@ -1,12 +1,12 @@
 package project;
 
 public class VMmachine {
-
-    String[] beverages={"pocari","coke","cider","welch's","vita500"};
-    int[] stocks = new int[]{15,15,15,15,15};
-
-    void consume(int idx, int num){
-        if(stocks[idx]<demand) System.out.println(beverages[idx]+"is not enough");
-    }
+//
+//    String[] beverages={"pocari","coke","cider","welch's","vita500"};
+//    int[] stocks = new int[]{15,15,15,15,15};
+//
+//    void consume(int idx, int num){
+//        if(stocks[idx]<demand) System.out.println(beverages[idx]+"is not enough");
+//    }
 
 }
