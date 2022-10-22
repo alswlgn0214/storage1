4f70594d5b4319c50098dbd420aa7fa818c06a53
hsol19 <kimhgst919@naver.com>
2022-10-22 12:32:29 +0900

    Date

--- src/ch12/main.java
+++ src/ch12/main.java
@@ -12,18 +12,24 @@ public class main {
         thatDay.set(Calendar.MONTH,4);
         thatDay.set(Calendar.DATE,1);
 
+        int year = thatDay.get(Calendar.YEAR);
+
         int num = thatDay.get(Calendar.DAY_OF_YEAR);
 
         Date d = new Date();
         Calendar today = Calendar.getInstance();
         today.setTime(d);
         System.out.println(d);
+
+        int year1 = today.get(Calendar.YEAR);
         int num1 = today.get(Calendar.DAY_OF_YEAR);
+
+        int sum = 365 * (year1 - year) + (num1 - num);
 
         boolean a = today.after(thatDay);
 
         if(a == true){
-            System.out.println(num1 - num);
+            System.out.println(sum);
 
         }
         else System.out.println("X");
